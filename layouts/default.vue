881063823eb098d984368549fe4498aa5bb9b740
paulmarniquet <paul.marniquet@epitech.eu>
2025-01-16 15:15:08 +0100

    feature: added logo

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,18 +3,21 @@
 </script>
 
 <template>
-    <header class="flex items-center justify-between p-4 max-w-6xl mx-auto bg-white">
-      <slot name="header" class="flex items-center space-x-4">
-        <NuxtImg src="~/assets/logo.png" alt="ogeez" width="32" height="32" />
-      </slot>
+  <header class="flex items-center justify-between p-4 max-w-6xl mx-auto bg-white">
+    <slot name="header" class="flex items-center space-x-4">
+      <div class="flex items-center space-x-3">
+        <img src="@/assets/logo.png" alt="ogeez" class="w-8 h-8"/>
+        <h1 class="title text-xl">ogeez</h1>
+      </div>
+    </slot>
 
-      <NuxtLink to="https://github.com/paulmarniquet/ogeez" class="text-primary hover:text-primary-600">
-        <UIcon name="grommet-icons:github" class="w-8 h-8 text-primary-500" />
-      </NuxtLink>
-    </header>
-    <main>
-      <slot/>
-    </main>
+    <NuxtLink to="https://github.com/paulmarniquet/ogeez" class="text-primary hover:text-primary-600">
+      <UIcon name="grommet-icons:github" class="w-8 h-8 text-primary-500"/>
+    </NuxtLink>
+  </header>
+  <main>
+    <slot/>
+  </main>
 </template>
 
 
