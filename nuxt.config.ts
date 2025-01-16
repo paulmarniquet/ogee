4f9b82e4dab2e6c3bcc1b35ba5900a1cf6245d1e
paulmarniquet <paul.marniquet@epitech.eu>
2025-01-16 15:15:49 +0100

    feature: added logo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2024-11-01',
-    modules: ['@nuxt/ui', '@nuxt/image'],
+    modules: [
+        '@nuxt/ui',
+        ['@nuxtjs/google-fonts',
+        {
+            families: {
+                'Righteous': '200..700',
+            },
+        },
+    ],],
     css: ['~/assets/css/main.css'],
 })
