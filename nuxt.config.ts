f297a49b7df38d574c9d2dd8a2fd1647d6fbb570
paulmarniquet <paul.marniquet@epitech.eu>
2025-01-16 16:28:00 +0100

    feature: metadata added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            title: 'ogeez - Sublimate your presence',
+            meta: [
+                { name: 'description', content: 'ogeez is an open graph generator that helps you create beautiful images for your social media posts.' },
+                { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+                { name: 'author', content: 'Paul Marniquet' },
+            ],
+            link: [
+                { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+            ],
+        },
+    },
     compatibilityDate: '2024-11-01',
     modules: [
         '@nuxt/ui',
