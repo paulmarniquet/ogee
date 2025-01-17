3cf0760356813bc9c31f764abe3ec6c388442018
paulmarniquet <paul.marniquet@epitech.eu>
2025-01-17 15:27:43 +0100

    fix: deleted dark theme and also added animations on color picker

--- components/properties/PropGradient.vue
+++ components/properties/PropGradient.vue
@@ -13,6 +13,11 @@ defineProps({
   },
 });
 
+const items = ref([
+  {label: 'Gradient', key: 'gradient'},
+  {label: 'Colors', key: 'colors'},
+]);
+
 const emit = defineEmits(['update:gradient']);
 
 // Fonction pour sélectionner un dégradé
@@ -24,6 +29,8 @@ const selectGradient = (gradient: any) => {
   };
   emit('update:gradient', updatedGradient);
 };
+
+const activeTab = ref(items.value[0].key); // Détermine l'onglet actif
 </script>
 
 <template>
@@ -31,43 +38,50 @@ const selectGradient = (gradient: any) => {
   <div>
     <label class="block text-sm font-medium text-gray-700 mb-4">Background</label>
 
-    <div>
-      <label class="block text-sm mb-4 font-medium text-gray-700">Gradient</label>
-      <div class="flex flex-wrap gap-2">
-        <UButton
-            v-for="(color, index) in gradientColors"
-            :key="index"
-            @click="selectGradient(color)"
-            class="w-8 h-8 rounded-lg cursor-pointer"
-            :style="{
-        background: `linear-gradient(to right, ${color.colorStops.join(', ')})`
-      }"
-        />
-      </div>
+    <UTabs
+        color="neutral"
+        :items="items" v-model:active="activeTab" class="w-full">
+      <template #content="{ item }">
 
-      <label class="block text-sm mt-4 mb-2 font-medium text-gray-700">Angle</label>
-      <UInput
-          type="number"
-          v-model="gradient.angle"
-          class="mt-1 block text-primary w-full rounded-md"
-      />
-    </div>
+        <div v-if="item.key === 'gradient'">
+          <div class="flex flex-wrap gap-2">
+            <UButton
+                v-for="(color, index) in gradientColors"
+                :key="index"
+                @click="selectGradient(color)"
+                class="w-8 h-8 rounded-lg cursor-pointer hover:scale-105
+                 transition duration-300 ease-in-out focus:outline-none focus:border-2 focus:border-primary-600
+                 focus:hover:scale-100"
+                :style="{
+                background: `linear-gradient(to right, ${color.colorStops.join(', ')})`
+              }"
+            />
+          </div>
 
-    <div>
-      <label class="block text-sm mt-4 mb-2 font-medium text-gray-700">Basic Color</label>
-      <div class="flex flex-wrap gap-2">
-        <UButton
-            v-for="(color, index) in solidColors"
-            :key="index"
-            @click="selectGradient({ colorStops: [color, color] })"
-            class="w-8 h-8 rounded-lg cursor-pointer"
-            :style="{
-        background: color
-      }"
-        />
-      </div>
-    </div>
+          <label class="block text-sm mt-4 font-medium text-gray-700">Angle</label>
+          <div class="flex items-center justify-center gap-4">
+            <USlider :default-value="gradient.angle" :min="0" :max="360" v-model="gradient.angle" class="mt-2"/>
+            <span class="font-semibold">{{ gradient.angle }}°</span>
+          </div>
+        </div>
 
+        <div v-if="item.key === 'colors'">
+          <div class="flex flex-wrap gap-2">
+            <UButton
+                v-for="(color, index) in solidColors"
+                :key="index"
+                @click="selectGradient({ colorStops: [color, color] })"
+                class="w-8 h-8 rounded-lg cursor-pointer hover:scale-105
+                 transition duration-300 ease-in-out focus:outline-none focus:border-2 focus:border-primary-600
+                 focus:hover:scale-100"
+                :style="{
+                background: color
+              }"
+            />
+          </div>
+        </div>
+      </template>
+    </UTabs>
   </div>
 </template>
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    ui: {
+        colorMode: false
+    },
     app: {
         head: {
             title: 'ogeez - Sublimate Your Presence',
