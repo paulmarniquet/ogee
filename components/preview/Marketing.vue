2bc92b10ad5342097cf8e4aa97d52bc6da8fbe2a
paulmarniquet <paul.marniquet@epitech.eu>
2025-01-21 17:06:00 +0100

    feature: preview for first template

--- components/preview/Marketing.vue
+++ components/preview/Marketing.vue
@@ -2,15 +2,27 @@
 </script>
 
 <template>
-  <div class="flex flex-col gap-2 w-48 h-24 p-4 rounded-xl cursor-pointer hover:bg-gray-100 transition duration-300 ease-in-out">
-    <div class="flex aspect-video h-full w-full flex-col items-center justify-center">
-      <div class="bg-gray-200 h-4 w-4 rounded-full md:h-8 md:w-8"></div>
-      <div class="rounded-md bg-gray-200 mt-2 h-3 w-1/2 md:mt-3 md:h-4"></div>
-      <div class="rounded-md bg-gray-200 mt-1 h-2 w-3/4 md:mt-2 md:h-3"></div>
+  <div
+      class="flex flex-col gap-2 w-48 h-24 p-4 rounded-xl cursor-pointer hover:bg-gray-100 transition duration-300 ease-in-out overflow-hidden">
+
+    <div class="h-full w-full">
+      <!-- Petit cercle en haut -->
+      <div class="bg-gray-200 h-3 w-3 rounded-full md:h-6 md:w-6"></div>
+
+      <!-- Conteneur principal -->
+      <div class="rounded-3xl flex aspect-video h-full w-full justify-center space-x-2 md:space-x-4">
+        <!-- Section gauche -->
+        <div class="flex h-full w-1/2 flex-col space-y-1 pt-2 md:pt-4">
+          <div class="rounded-3xl bg-gray-200 h-1 w-1/3 md:h-2"></div>
+          <div class="rounded-3xl bg-gray-200 h-2 w-full md:h-4"></div>
+        </div>
+
+        <!-- Section droite -->
+        <div class="rounded-3xl bg-gray-200 h-2/3 w-1/2"></div>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-
 </style>
